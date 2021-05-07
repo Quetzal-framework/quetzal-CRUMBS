44927b70ad39fce042f0e7fa5ab7f849fe4c4abc
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-05-07 16:07:01 -0400

    ready por registering package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='quetzal-crumbs',
+      version='0.0.1',
+      description='Utility scripts for Quetzal-based programs',
+      author='Arnaud Becheler',
+      author_email='arnaud.becheler@gmail.com',
+      url='https://github.com/Becheler/quetzal-CRUMBS',
+      download_url='https://github.com/Becheler/quetzal-CRUMBS/tarball/0.0.1',
+      keywords=['quetzal', 'python', 'crumbs', 'coalescence'],
+      classifiers=[]
+     )
