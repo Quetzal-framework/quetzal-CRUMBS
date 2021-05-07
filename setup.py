ae7c6f214d143f856c1cdcd27adaa0a2fa45f7ca
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-05-07 16:18:28 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
 
-setup(name='quetzal-crumbs',
-      version='0.0.1',
-      description='Utility scripts for Quetzal-based programs',
-      author='Arnaud Becheler',
-      author_email='arnaud.becheler@gmail.com',
-      url='https://github.com/Becheler/quetzal-CRUMBS',
-      download_url='https://github.com/Becheler/quetzal-CRUMBS/tarball/0.0.1',
-      keywords=['quetzal', 'python', 'crumbs', 'coalescence'],
-      classifiers=[]
-     )
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="quetzal-crumbs",
+    version="0.0.1",
+    description='Utility scripts for Quetzal-based programs',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Becheler/quetzal-CRUMBS',
+    author='Arnaud Becheler',
+    author_email='arnaud.becheler@gmail.com',
+    packages=["crumbs"],
+    include_package_data=True
+)
