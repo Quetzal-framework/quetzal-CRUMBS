afdc0de16f45635fe299cbb75a7c63c0d4b22449
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-05-12 12:17:15 -0400

    test for raster works

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest, math
 from pathlib import Path
 from crumbs import sequence, bpp
+from osgeo import gdal
 
 class TestSequence(unittest.TestCase):
     def SetUp(self):
@@ -28,6 +29,34 @@ class TestBPP(unittest.TestCase):
         posterior = bpp.nb_species_posterior_probabilities(string)
         assert math.isclose(posterior[1], 0.0)
         assert math.isclose(posterior[2], 0.985)
+
+class TestTiff(unittest.TestCase):
+    def SetUp(self):
+        pass
+
+    def test_rasterIO(self):
+        # Open the file:
+        raster = gdal.Open(r'test_data/suitability.tif')
+        # Check type of the variable 'raster'
+        type(raster)
+        # Projection
+        raster.GetProjection()
+        # Dimensions
+        self.assertEqual(raster.RasterXSize, 240)
+        self.assertEqual(raster.RasterYSize, 168)
+        # Number of bands
+        self.assertEqual(raster.RasterCount, 1)
+        # Read the raster band as separate variable
+        band = raster.GetRasterBand(1)
+        # Compute statistics if needed
+        if band.GetMinimum() is None or band.GetMaximum()is None:
+            band.ComputeStatistics(0)
+        # Fetch metadata for the band
+        band.GetMetadata()
+        # Print only selected metadata:
+        self.assertEqual(band.GetNoDataValue(), -3.4e+38)
+        self.assertEqual(band.GetMinimum(), 0.15809911489487)
+        self.assertEqual(band.GetMaximum(), 0.78696364164352)
 
 if __name__=="__main__":
     unittest.main()
