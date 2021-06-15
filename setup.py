234c0ce90d2deb8ca5c0d50cdce5e3653246da7b
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-06-14 21:53:13 -0400

    publishing

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="quetzal-crumbs",
-    version="0.0.1",
+    version="0.0.2",
     description='Utility scripts for Quetzal-based programs',
     long_description=README,
     long_description_content_type="text/markdown",
