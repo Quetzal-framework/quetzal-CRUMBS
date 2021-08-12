f4ce78d3f77cd41a6af6fce11350bdd68510fbd5
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-08-12 19:11:57 -0400

    syntax

--- src/crumbs/simulate_sequences.py
+++ src/crumbs/simulate_sequences.py
@@ -4,8 +4,7 @@ import evolve
 
 # options
 parser = OptionParser()
-parser.add_option("--database", type="str", dest="database", help="path to database")
-parser.add_option("--table", type="str", dest="table", help="what simulation core table to use")
+parser.add_option("--database", type="str", dest="database", help="path to database - must contain a pods table")
 parser.add_option("--rowid", type="str", dest="rowid", help="what row in the database table")
 parser.add_option("--sequence_size", type="int", dest="sequence_size", help="sequence_size")
 parser.add_option("--scale_tree", type="float", dest="scale_tree", help="scale tree branch length")
@@ -15,19 +14,19 @@ parser.add_option("--output", type="str", dest="filename", help="output file nam
 conn = sqlite3.connect(options.database)
 
 try:
-    conn.execute("ALTER TABLE core4_pods ADD COLUMN scale_tree DOUBLE")
+    conn.execute("ALTER TABLE pods ADD COLUMN scale_tree DOUBLE")
 except Exception:
     pass
 
 try:
-    conn.execute("ALTER TABLE core4_pods ADD COLUMN sequence_size INTEGER")
+    conn.execute("ALTER TABLE pods ADD COLUMN sequence_size INTEGER")
 except Exception:
     pass
 
 cursor = conn.cursor()
 
 t = (options.rowid,)
-cursor.execute('SELECT newicks FROM core4_pods WHERE rowid=?', t)
+cursor.execute('SELECT newicks FROM pods WHERE rowid=?', t)
 record = cursor.fetchone()
 trees = record[0].split("\n\n")
 evolve(trees, options.sequence_size, options.scale_tree, options.filename)
