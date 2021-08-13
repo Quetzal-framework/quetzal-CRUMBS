ab3370b3ff83238a31a36ee9180b9a0e4a6d069f
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-08-12 23:18:51 -0400

    delete evolve.py

--- src/crumbs/simulate_phylip_sequences.py
+++ src/crumbs/simulate_phylip_sequences.py
@@ -79,7 +79,7 @@ def simulate_phylip_sequences(database, table, rowid, sequence_size, scale_tree,
         raise Runtime("Error when fetching row with rowid " + rowid + ". Make sure that row is defined in table using crumbs.get_simulations_rowids.")
     # newick formulas are separated by 2 Newline characters
     trees = record[0].split("\n\n")
-    evolve.newick_list_to_phylip_simulation(trees, sequence_size, scale_tree, output_file)
+    newick_list_to_phylip_simulation(trees, sequence_size, scale_tree, output_file)
     record_parameters(conn, )
     cursor.execute('''UPDATE quetzal_EGG_1 SET scale_tree = ? WHERE rowid = ?''', (scale_tree, rowid))
     cursor.execute('''UPDATE quetzal_EGG_1 SET sequence_size = ? WHERE rowid = ?''', (sequence_size, rowid))
