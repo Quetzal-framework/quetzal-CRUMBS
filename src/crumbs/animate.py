76a81b7895f6304cce37920606e2ea2684a76430
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-02-02 15:59:17 -0500

    fix: problem with the warp scale, can't be specified along with extent

--- src/crumbs/animate.py
+++ src/crumbs/animate.py
@@ -8,6 +8,17 @@ import imageio
 from tqdm import tqdm
 import numpy
 
+def summary(dataset):
+    print(" - no data value: \t", dataset.nodata )
+    print(" - crs:\t", dataset.crs)
+    print(" - ", dataset.bounds)
+    pxsz, pysz = dataset.res
+    print(" - pixel size X: \t", pxsz, "unit:", dataset.crs.linear_units)
+    print(" - pixel size Y: \t", pysz, "unit:", dataset.crs.linear_units)
+    print(" - width: \t", dataset.width)
+    print(" - height: \t", dataset.height)
+    return
+
 def get_band(Z, i):
     """ Return the ith band of the (possibly masked) Z 3D array: if masked, fills masked values with nan.
     """
@@ -18,56 +29,112 @@ def get_band(Z, i):
 
 from mayavi import mlab
 
+def get_points(shapefile):
+    from shapely.geometry import shape
+    import fiona
+    lons, lats, years = ([] for i in range(3))
+    with fiona.open(shapefile, mode='r') as source:
+        print(source.crs)
+        for feature in source:
+            try:
+                g = feature["geometry"]
+                assert g["type"] == "Point"
+                lon, lat = g["coordinates"]
+                year = feature['properties']['Year']
+                lons.append(float(lon))
+                lats.append(float(lat))
+                years.append(int(year))
+            except IOError:
+                logging.exception("Error processing feature %s:", feature["id"])
+    return lons, lats, years
+
+def make_X_Y_Z(raster_src):
+    Z = raster_src.read(masked=True)
+    nrows, ncols = Z[0].shape
+    # the value returned by bounds is a tuple: (lower left x, lower left y, upper right x, upper right y)
+    x = numpy.linspace(raster_src.bounds[0], raster_src.bounds[2], ncols)
+    y = numpy.linspace(raster_src.bounds[1], raster_src.bounds[3], nrows)
+    X, Y = numpy.meshgrid(x, y)
+    return X, Y, Z
+
+def plot_axes():
+    import numpy as np
+    xx = yy = zz = np.arange(start = 0.0, stop = 1300, step = 1)
+    xy = xz = yx = yz = zx = zy = np.zeros_like(xx)
+    lensoffset = 0
+    mlab.plot3d(yx, yy + lensoffset, yz, line_width=10, tube_radius=10)
+    mlab.plot3d(zx, zy + lensoffset, zz, line_width=10, tube_radius=10)
+    mlab.plot3d(xx, xy + lensoffset, xz, line_width=10, tube_radius=10)
+
+def decorate(surface, vmin, vmax):
+    # Custom axes because mayavi defaults suck
+    plot_axes()
+    # Vertical colorbar
+    mlab.colorbar(orientation='vertical')
+    # Sets nan pixels to white
+    surface.module_manager.scalar_lut_manager.lut.nan_color = 0, 0, 0, 0
+    # Fix colorbar to global max/min
+    surface.module_manager.scalar_lut_manager.use_default_range = False
+    surface.module_manager.scalar_lut_manager.data_range = numpy.array([vmin, vmax])
+    surface.update_pipeline()
+
 @mlab.animate(delay=10, ui=False)
-def update_animation(Z, surface, writer, vmin, vmax, DDD=False):
+def update_animation(Z, surface, points, writer, vmin, vmax, DDD=False):
+
     f = mlab.gcf()
     t = 2.0
+
     increment = 0.1 if DDD is True else 1.0
-    sequence = numpy.arange(2.0, float(Z.shape[0]), increment)
+
+    sequence = numpy.arange(t, float(Z.shape[0]), increment)
+
     for i in tqdm(range(len(sequence))):
-        # get the current lut manager
-        lut_manager = mlab.colorbar(orientation='vertical')
-        surface.module_manager.scalar_lut_manager.use_default_range = False
-        surface.module_manager.scalar_lut_manager.data_range = numpy.array([vmin, vmax])
-        surface.update_pipeline()
+        mlab.points3d(points[0], points[1], points[2])
+        decorate(surface, vmin, vmax)
+
         if(DDD is True):
             f.scene.camera.azimuth(1)
             f.scene.render()
-        t += increment
 
+        t += increment
         surface.mlab_source.scalars = get_band(Z, int(t)-1)
         writer.append_data(mlab.screenshot())
         yield
 
-def animate(inputRaster, vmin=None, vmax=None, output=None, gbif=None, DDD=False, warp_scale=1.0):
+def animate(inputRaster, vmin=None, vmax=None, output=None, gbif_occurrences=None, DDD=False, warp_scale=1.0):
     output = 'animation.gif' if output is None else output
-
     with rasterio.open(inputRaster) as source:
+        print("- Source dataset:", inputRaster)
+        summary(source)
 
-        Z = source.read(masked=True)
-
-        nrows, ncols = Z[0].shape
-        x = numpy.linspace(source.bounds[0], source.bounds[2], ncols)
-        y = numpy.linspace(source.bounds[1], source.bounds[3], nrows)
-        X, Y = numpy.meshgrid(x, y)
+        # Elevation surface
+        X, Y, Z = make_X_Y_Z(source)
 
         if vmax is None: vmax = numpy.amax(Z)
         if vmin is None: vmin = numpy.amin(Z)
 
+        lons, lats, years = get_points(gbif_occurrences)
+        points_ys, points_xs = rasterio.transform.rowcol(transform=source.transform, xs=lons, ys=lats)
+
+        extent = [0, Z.shape[2] - 1, 0, Z.shape[1] - 1, vmin/warp_scale, vmax/warp_scale]
+
         with imageio.get_writer(output, mode='I') as writer:
 
-            if(DDD is False):
-                surface = mlab.imshow(get_band(Z, 0), colormap='viridis')
-                # view along z axis
+            if DDD is True:
+                surface = mlab.surf(get_band(Z, 0), colormap='viridis', representation='wireframe', extent = extent)
+                decorate(surface, vmin, vmax)
+                points_zs = [ Z[Z.shape[0]-1, points_ys[i], points_xs[i]]/warp_scale for i in range(len(points_xs)) ]
+                print(points_zs)
+            else:
+                surface = mlab.imshow(get_band(Z, 0), colormap='viridis', extent = extent)
+                decorate(surface, vmin, vmax)
+                # view surface along z axis (2D)
                 mlab.view(0,0)
-            elif(DDD is True):
-                surface = mlab.surf(get_band(Z, 0), colormap='viridis', warp_scale=warp_scale)
+                # Observational points have no elevation
+                points_zs = [0] * len(points_xs)
 
-            # Sets nan pixels to white
-            surface.module_manager.scalar_lut_manager.lut.nan_color = 0, 0, 0, 0
-
-            a = update_animation(Z, surface, writer, vmin, vmax, DDD)
-
+            points = [points_xs, points_ys, points_zs]
+            a = update_animation(Z, surface, points, writer,  vmin, vmax, DDD)
             mlab.show()
 
 def main(argv):
@@ -84,7 +151,7 @@ def main(argv):
      vmin=options.min,
      vmax=options.max,
      output=options.output,
-     gbif=options.gbif,
+     gbif_occurrences=options.gbif,
      DDD=options.DDD,
      warp_scale=options.warp_scale)
 
