a7fe88f0fc88d5ad9ed29b80600c27cd64eef1da
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-02-21 18:32:55 -0500

    fix: fix the problem of trying to find minimum in GDAL dataset: just use rasterio

--- src/crumbs/sample.py
+++ src/crumbs/sample.py
@@ -12,22 +12,20 @@ def uniform_real(min, max):
 from osgeo import gdal
 import numpy as np
 
-def uniform_latlon_above_minimum(raster_path):
+def uniform_latlon_above_minimum(raster_path, band):
     from os.path import exists
+    import rasterio
     assert exists(path_to_file), 'File doest not exists:' + raster_path
+    assert band >= 0 'Band index must be integer >= 0'
 
-    raster = gdal.Open(raster_path, gdal.GA_ReadOnly)
-    band = raster.GetRasterBand(1)
-    array = band.ReadAsArray().astype(float)
-    (y_index, x_index) = np.nonzero(array >= band.GetMinimum())
-    (upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size) = raster.GetGeoTransform()
-    x_coords = x_index * x_size + upper_left_x + (x_size / 2) #add half the cell size
-    y_coords = y_index * y_size + upper_left_y + (y_size / 2) #to centre the point
-    assert (x_coords.size == y_coords.size)
-    i = random.randint(0, x_coords.size -1)
-    latlon = list()
-    latlon.append(y_coords[i])
-    latlon.append(x_coords[i])
+    with rasterio.open(raster_path) as src:
+        masked = src.read(1, masked=True)
+        assert band < src.count 'Dataset has only' + src.count 'bands. Can not sample band index ' + band
+        nb_sample = 1
+        cols, rows = random_sample_from_masked_array(masked, nb_sample)
+        xs, ys = rasterio.transform.xy(src.transform, rows, cols)
+
+    latlon = list(ys, xs)
     return latlon
 
 def uniform_latlon(raster_path):
