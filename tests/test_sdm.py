e9c2f2cfe92424826c01b037fd31972dc4798ed4
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-05-31 20:18:22 -0400

    test: adding context file for proper import

--- tests/test_sdm.py
+++ tests/test_sdm.py
@@ -1,28 +1,22 @@
 # avoid Module not found, see https://gideonbrimleaf.github.io/2021/01/26/relative-imports-python.html
 import unittest
 import sys
-sys.path.append("..")
 
-def delete_folder(pth) :
-    import pathlib
-
-    for sub in pth.iterdir() :
-        if sub.is_dir() :
-            delete_folder(sub)
-        else :
-            sub.unlink()
-    pth.rmdir() # if you just want to delete the dir content but not the dir itself, remove this line
-
+from .context import src.crumbs
+from .utils import delete_folder
 
 class TestSDM(unittest.TestCase):
 
     def setUp(self):
+        self.sampling_points = "data/test_points/test_points.shp"
+        self.occurrences_filename = "occurrences"
+
+    def test_fit_and_extrapolate(self):
 
         from src.crumbs.sdm import SDM
         from src.crumbs.get_gbif import search_gbif
 
-        self.sampling_points = "data/test_points/test_points.shp"
-        self.occurrences_filename = "occurrences"
+
 
         search_gbif(scientific_name='Heteronotia binoei',
                              points=self.sampling_points,
@@ -34,7 +28,7 @@ class TestSDM(unittest.TestCase):
                              shapefile= self.occurrences_filename + ".shp")
 
 
-        self.sdm = SDM(
+        sdm = SDM(
             scientific_name='Heteronotia binoei',
             presence_shapefile = self.occurrences_filename + ".shp",
             nb_background_points = 30,
@@ -43,12 +37,9 @@ class TestSDM(unittest.TestCase):
             buffer = 1.0
             )
 
-    def test_fit_sdm(self):
-        self.sdm.fit_on_present_data()
-
-    def test_extrapolate(self):
-        self.sdm.load_classifiers_and_extrapolate(20)
-        self.sdm.load_classifiers_and_extrapolate(19)
+        sdm.fit_on_present_data()
+        sdm.load_classifiers_and_extrapolate(20)
+        sdm.load_classifiers_and_extrapolate(19)
 
     def tearDown(self):
         from pathlib import Path
@@ -59,13 +50,16 @@ class TestSDM(unittest.TestCase):
             p.unlink()
 
         # Removing all persistence files generated
-        for p in Path(".").glob( "persist-" + "*.joblib"):
+        for p in Path(".").glob( "model-persistence" + "*.joblib"):
+            p.unlink()
+
+        # Removing all landscape files generated
+        for p in Path(".").glob( "chelsa-landscape" + "*.tif"):
             p.unlink()
 
         # Removing SDM input and outputs folder
-        # delete_folder(Path("sdm_inputs"))
-        # delete_folder(Path("sdm_outputs"))
-        # delete_folder(Path(self.cropped_file_dir))
+        delete_folder(Path("model-averaging"))
+        delete_folder(Path("model-imputation"))
 
 if __name__=="__main__":
     unittest.main()
