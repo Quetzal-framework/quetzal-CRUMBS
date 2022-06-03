9f889312f22b479f7140072bf3e04d25fc3b8986
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-06-02 22:17:04 -0400

    fix: forgot to add autosummary generate is true

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ extensions = [
     'sphinx.ext.autosummary'
 ]
 
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
