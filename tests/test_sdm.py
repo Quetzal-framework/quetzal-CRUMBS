cbdac6c5311d82b3d9d4b530e3746e2ae71cb1f5
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-06-30 19:06:16 -0400

    fix: tests teardown

--- tests/test_sdm.py
+++ tests/test_sdm.py
@@ -3,61 +3,61 @@ import pytest
 from . context import crumbs
 from . utils import delete_folder
 
-class TestSDM():
-
-    def setup_method(self, test_method):
-        self.sampling_points = "tests/data/test_points/test_points.shp"
-        self.occurrences_filename = "occurrences"
 
 
-    def teardown_method(self, test_method):
-        from pathlib import Path
-        import glob
+def teardown_method(self, test_method):
+    from pathlib import Path
+    import glob
 
-        # Removing all occurrences files generated
-        for p in Path(".").glob( self.occurrences_filename + ".*"):
-            p.unlink()
+    # Removing all occurrences files generated
+    for p in Path(".").glob( self.occurrences_filename + ".*"):
+        p.unlink()
 
-        # Removing sdm pickled
-        Path("my_sdm.bin").unlink()
+    # Removing sdm pickled
+    pickled = Path("my_sdm.bin")
+    if pickled.is_file():
+        pickled.unlink()
 
-        # Removing SDM input and outputs folder
-        delete_folder(Path("chelsa-landscape"))
-        delete_folder(Path("model-persistence"))
-        delete_folder(Path("model-averaging"))
-        delete_folder(Path("model-imputation"))
+    # Removing SDM input and outputs folder
+    delete_folder(Path("chelsa-landscape"))
+    delete_folder(Path("model-persistence"))
+    delete_folder(Path("model-averaging"))
+    delete_folder(Path("model-imputation"))
 
 
-    @pytest.mark.slow
-    def test_fit_and_extrapolate(self):
+@pytest.mark.slow
+def test_fit_and_extrapolate(tmp_path):
 
-        from crumbs.sdm.sdm import SDM
-        from crumbs.gbif.request import request
-        import pickle
+    from crumbs.sdm.sdm import SDM
+    from crumbs.gbif.request import request
+    import pickle
 
-        request(scientific_name='Heteronotia binoei',
-                             points=self.sampling_points,
-                             buffer=1.0,
-                             all=False,
-                             limit=50,
-                             year='1950,2022',
-                             csv_file= self.occurrences_filename + ".csv",
-                             shapefile= self.occurrences_filename + ".shp")
+    sampling_points = "tests/data/test_points/test_points.shp"
+    occurrences_filename = "occurrences"
 
-        my_sdm = SDM(
-            scientific_name='Heteronotia binoei',
-            presence_shapefile = self.occurrences_filename + ".shp",
-            nb_background_points = 30,
-            variables = ['dem','bio01'],
-            chelsa_time_IDs = [20],
-            buffer = 1.0
-            )
+    request(scientific_name='Heteronotia binoei',
+                         points=self.sampling_points,
+                         buffer=1.0,
+                         all=False,
+                         limit=50,
+                         year='1950,2022',
+                         csv_file= self.occurrences_filename + ".csv",
+                         shapefile= self.occurrences_filename + ".shp")
 
-        my_sdm.fit_on_present_data()
+    my_sdm = SDM(
+        scientific_name='Heteronotia binoei',
+        presence_shapefile = self.occurrences_filename + ".shp",
+        nb_background_points = 30,
+        variables = ['dem','bio01'],
+        chelsa_time_IDs = [20],
+        buffer = 1.0
+        )
 
-        with open("my_sdm.bin","wb") as f:
-            pickle.dump(my_sdm, f)
+    my_sdm.fit_on_present_data()
 
-        with open("my_sdm.bin","rb") as f:
-            my_saved_sdm = pickle.load(f)
-            my_saved_sdm.load_classifiers_and_extrapolate(20)
+    with open("my_sdm.bin","wb") as f:
+        pickle.dump(my_sdm, f)
+
+    with open("my_sdm.bin","rb") as f:
+        my_saved_sdm = pickle.load(f)
+        my_saved_sdm.load_classifiers_and_extrapolate(20)
